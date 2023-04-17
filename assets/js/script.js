ce413388dba42a162063c87a3f3ae281a1ee1b5c
Oscar Lundsör <oscarlundsor@gmail.com>
2023-04-17 19:12:25 +0000

    Played around with some script that I found on css-tricks.com

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 document.addEventListener("submit", function() {
+    event.preventDefault();
     let submitButton = document.getElementsByClassName("submit");
     
-    
+    let compoundLegArray = ["Deadlift", "Squat"];
+    let num1exercise = Math.floor(Math.random(compoundLegArray))
+
 })
