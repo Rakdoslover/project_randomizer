1df3630897f98aadd8e44f829a6490cdbaf492a5
Oscar Lundsör <oscarlundsor@gmail.com>
2023-04-20 09:32:11 +0000

    solved a problem regarding the If statements in the JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,63 @@
 // Targets the id:s for boxes
 let squatEx = document.getElementById("squat");
 let deadEx = document.getElementById("deadlift");
+let calfEX = document.getElementById("calf-raise");
+let kbswingEx = document.getElementById("kb-swing");
+let rlungeEx = document.getElementById("rear-lunge");
 let ttbEx = document.getElementById("toestobar");
 let benchPressEx = document.getElementById("bench-press");
 let plankEx = document.getElementById("plank-weighted");
-
+let chins = document.getElementById("chin-ups");
 
 /**
- * Function to remove all boxes, not decided yet if 
- * it's gonna be one for each instance or one that controls everything.
+ * Here below we have the variables for the toggle functions further down.
+ * Found the tips for the toggle function on Stackoverflow.com in a
+ * comment on changing the color of a div by pressing it.
  */
-function resetLegs(event) {
-    squatEx.style.display = "none";
-    deadEx.style.display = "none";
-}
+var toggleLegs = document.getElementById("create-remove-legs");
+toggleLegs = false;
+var togglePress = document.getElementById("create-remove-press");
+togglePress = false;
+var togglePull = document.getElementById("create-remove-pull");
+togglePull = false;
 
-function resetCore(event) {
-    ttbEx.style.display = "none";
-    plankEx.style.display = "none";
-}
+/**
+ * The functions for the toggle.
+ */
+// Function legs
+function showHideLegs() {
+    if (toggleLegs === true) {
+        document.getElementById('squat').style.display = "inline-block";
+        document.getElementById('deadlift').style.display = "inline-block";
+        document.getElementById('calf-raise').style.display = "inline-block";
+        document.getElementById('kb-swing').style.display = "inline-block";
+        document.getElementById('rear-lunge').style.display = "inline-block";
+    } else {
+        document.getElementById('squat').style.display = "none";
+        document.getElementById('deadlift').style.display = "none";
+        document.getElementById('calf-raise').style.display = "none";
+        document.getElementById('kb-swing').style.display = "none";
+        document.getElementById('rear-lunge').style.display = "none";
+    }
+    toggleLegs = !toggleLegs;
+};
 
-function resetPress(event) {
-    benchPressEx.style.display = "none";
-}
+// Function press
+function showHidePress() {
+    if (togglePress === true) {
+        document.getElementById('bench-press').style.display = "inline-block";
+    } else {
+        document.getElementById('bench-press').style.display = "none";
+    }
+    togglePress = !togglePress;
+};
 
-// Funtion for creating Leg exercises
-document.getElementById("create-legs").onclick = function createLegs() {
-    document.getElementById("squat").style.display = "inline-block";
-    document.getElementById("deadlift").style.display = "inline-block";
-}
-
-// Function for creating Ab exercises
-document.getElementById("create-core").onclick = function createCore() {
-    document.getElementById("toestobar").style.display = "inline-block";
-    document.getElementById("plank-weighted").style.display = "inline-block";
-
-}
-
-// Function for creating Press exercises
-document.getElementById("create-press").onclick = function createPress() {
-    document.getElementById("bench-press").style.display = "inline-block";
-}
+// Function pull
+function showHidePull() {
+    if (togglePull === true) {
+        document.getElementById('chin-ups').style.display = "inline-block";
+    } else {
+        document.getElementById('chin-ups').style.display = "none";
+    }
+    togglePull = !togglePull;
+};
